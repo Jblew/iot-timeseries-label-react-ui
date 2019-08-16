417f6959dc929db3ef1dcd64c1cbcbb55a85535d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-16 17:49:17 +0200

    feat: basic DrawerLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-import Button from "@material-ui/core/Button";
-import "./App.css";
+
+import DrawerLayour from "./components/layout/DrawerLayout";
+import Menu from "./components/Menu";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-                <p>
-                    <Button variant="contained" color="primary">
-                        Hello World
-                    </Button>
-                </p>
-            </header>
-        </div>
+        <DrawerLayour menu={<Menu />}>
+            <p>Some content</p>
+        </DrawerLayour>
     );
 }
 
